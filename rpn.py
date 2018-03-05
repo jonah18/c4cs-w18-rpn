1dd2283026e6afe8f54a90d4636607fb44aa4866
jonah18 <jonahmg@umich.edu>
2018-03-05 10:57:00 -0500

    Added coloring

--- rpn.py
+++ rpn.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+class bcolors:
+    PROMPT = '\033[94m'
+    POS = '\033[92m'
+    INPUT = '\033[93m'
+    NEG = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def calculate(arg):
 	stack = list()
 	for token in arg.split():
@@ -18,7 +27,10 @@ def calculate(arg):
 
 def main():
 	while True:
-		print(calculate(input("rpn calc> ")))
-
+		val = calculate(input(bcolors.PROMPT + "rpn calc> " + bcolors.INPUT))
+		if val >= 0:
+			print(bcolors.POS + str(val))
+		elif val < 0:
+			print(bcolors.NEG + str(val))
 if __name__ == '__main__':
 	main()
